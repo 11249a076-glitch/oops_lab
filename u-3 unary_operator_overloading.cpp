8dc8e8acf28d36dc9e812e184867dd4100828f5f
Neeraj <11249a076@kanchiuniv.ac.in>
2025-11-21 19:35:59 +0530

    Update u-3 unary_operator_overloading.cpp

--- u-3 unary_operator_overloading.cpp
+++ u-3 unary_operator_overloading.cpp
@@ -1,1 +1,40 @@
+/*
+Aim:
+To overload the unary minus operator to convert Celsius to Fahrenheit.
 
+Algorithm:
+1. Create class Temperature with private celsius variable.
+2. Take Celsius input from user.
+3. Overload unary operator -().
+4. When -object is used, convert Celsius to Fahrenheit.
+5. Display result.
+6. End program.
+
+Output:
+Enter temperature in Celsius: 37
+Temperature in Fahrenheit: 98.6
+*/
+
+#include <iostream>
+using namespace std;
+
+class Temperature {
+    float celsius;
+public:
+    void getData() {
+        cout << "Enter temperature in Celsius: ";
+        cin >> celsius;
+    }
+    void operator-() {
+        float fahrenheit = (celsius * 9/5) + 32;
+        cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+    }
+};
+
+int main() {
+    Temperature t;
+    t.getData();
+    -t;
+
+    return 0;
+}
